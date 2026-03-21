2c3508f4e1b79ebcf6f60f209600fdcf88bf0950
PrashastRawat <your@email.com>
2026-03-21 23:51:17 +0530

    fix: Docker runtime, update client.js

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "http://127.0.0.1:8000" });
+const api = axios.create({ baseURL: import.meta.env.VITE_API_URL || "http://127.0.0.1:8000", });
 
 // Attach token to every request automatically
 api.interceptors.request.use((config) => {
